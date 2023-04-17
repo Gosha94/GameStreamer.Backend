b55d6f8545bbd8557aeae4cb62ce3de9198f3815
Georgy <gerizch@rambler.ru>
2023-04-17 11:40:51 +0300

    added roomName domain test

--- tests/GameStreamer.Domain.Tests/ValueObjects/RoomNameShould.cs
+++ tests/GameStreamer.Domain.Tests/ValueObjects/RoomNameShould.cs
@@ -1,5 +1,32 @@
-﻿namespace GameStreamer.Domain.Tests.ValueObjects;
+﻿using FluentAssertions;
+using GameStreamer.Domain.Errors;
+using GameStreamer.Domain.ValueObjects;
+
+namespace GameStreamer.Domain.Tests.ValueObjects;
 
 public class RoomNameShould
 {
+    [Fact]
+    public void ReturnsFailure_WhenRoomNameIsEmpty()
+    {
+        // Arrange
+
+        // Act
+        var actualResult = RoomName.Create(string.Empty);
+
+        // Assert
+        actualResult.Error.Should().Be(DomainErrors.EmptyRoomName);
+    }
+
+    [Fact]
+    public void ReturnsFailure_WhenRoomNameIsTooLong()
+    {
+        // Arrange
+
+        // Act
+        var actualResult = RoomName.Create("TooLongRoomNameEverEverCreatedForTest");
+
+        // Assert
+        actualResult.Error.Should().Be(DomainErrors.TooLongRoomName);
+    }
 }
